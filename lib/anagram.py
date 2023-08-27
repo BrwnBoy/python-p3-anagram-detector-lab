0e0a8058fa566e0d1938d903f45ebdc6100da4a9
Brwni <brianmm665@gmial.com>
2023-08-27 16:42:20 +0300

    Done with assignment

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,10 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+        self.sorted_word = sorted(word)
+        
+    def match(self, candidates):
+        return [candidate for candidate in candidates if sorted(candidate) == self.sorted_word and candidate.lower() != self.word.lower()]
+    
+listen = Anagram("listen")
+print(listen.match(['enlists', 'google', 'inlets', 'banana'])) # Output: ['inlets]
